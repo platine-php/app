eafcba3a092e99f558f5d6fedd6a042bebbf8f4d
Tony NGUEREZA <nguerezatony@gmail.com>
2021-09-20 00:53:59 +0100

    Add log level environment variable

--- config/logging.php
+++ config/logging.php
@@ -3,13 +3,13 @@
     use Platine\Logger\LogLevel;
 
     return [
-        'level' => LogLevel::ERROR,
+        'level' => env('PL_LOG_LEVEL', LogLevel::DEBUG),
         'driver' => 'file',
         'handlers' => [
             'file' => [
                 'path' => env('PL_LOG_PATH', __DIR__ . '/../storage/tmp/logs'),
                 'prefix' => 'app.',
-                'level' => LogLevel::ERROR,
+                'level' => env('PL_LOG_FILE_LEVEL', LogLevel::DEBUG),
             ]
         ]
     ];
