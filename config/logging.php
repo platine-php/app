239ecace7d7e68a98655d1f6d5c0f3dc223510fe
NGUEREZA Tony <nguerezatony@gmail.com>
2023-05-11 05:48:13 +0100

    Update logger configuration to include support of use of timestamp (full time)

--- config/logging.php
+++ config/logging.php
@@ -3,8 +3,9 @@
     use Platine\Logger\LogLevel;
 
     return [
-        'driver' => env('PL_LOG_DRIVER', 'file'),
-        'level'  => env('PL_LOG_LEVEL', LogLevel::DEBUG),
+        'driver'     => env('PL_LOG_DRIVER', 'file'),
+        'level'      => env('PL_LOG_LEVEL', LogLevel::DEBUG),
+        'timestamp'  => env('PL_LOG_TIMESTAMP', false),
         'handlers' => [
             'file' => [
                 'path'   => env('PL_LOG_PATH', __DIR__ . '/../storage/tmp/logs'),
