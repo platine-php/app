c1063ecbecb854e1a6d8bef5d881ca15028b99f2
NGUEREZA Tony <nguerezatony@gmail.com>
2023-06-22 17:21:32 +0100

    Add application host config key

--- config/app.php
+++ config/app.php
@@ -6,6 +6,7 @@
         'env' => env('PL_APP_ENV', 'dev'),
         'version' => env('PL_APP_VERSION', '1.0.0-dev'),
         'url' => env('PL_APP_URL', ''),
+		'host' => env('PL_APP_HOST', ''), //without the last "/"
         'base_path' => env('PL_APP_BASE_PATH', ''),
         'static_dir' => 'static',
         'response_chunck_size' => 4096,
