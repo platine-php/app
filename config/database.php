13835119811ad6263221963ab68bef5c4af25499
NGUEREZA Tony <nguerezatony@gmail.com>
2023-08-03 14:33:11 +0100

    Minor change

--- config/database.php
+++ config/database.php
@@ -1,14 +1,14 @@
 <?php
 
     return [
-        'default' => env('PL_DB_DRIVER', 'sqllite'),
+        'default' => env('PL_DB_DRIVER', 'mysql'),
 
         'connections' => [
             'mysql' => [
                 'driver' => 'mysql',
                 'database' => env('PL_DB_NAME', ''),
                 'hostname' => env('PL_DB_HOST', ''),
-                'port' => env('PL_DB_PORT', 0, 'int'),
+                'port' => env('PL_DB_PORT', 3306, 'int'),
                 'username' => env('PL_DB_USER', 'root'),
                 'password' => env('PL_DB_PASSWORD', ''),
                 'persistent' => false,
