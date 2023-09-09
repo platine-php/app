9e31f5b6e7ff7918bcb2428d2e010a43c06a4c4a
NGUEREZA Tony <nguerezatony@gmail.com>
2023-09-09 07:28:38 +0100

    Add security policies

--- config/middlewares.php
+++ config/middlewares.php
@@ -4,10 +4,12 @@ use Platine\App\Http\Middleware\DemoMiddleware;
 use Platine\Framework\Http\Middleware\ErrorHandlerMiddleware;
 use Platine\Framework\Http\Middleware\RouteDispatcherMiddleware;
 use Platine\Framework\Http\Middleware\RouteMatchMiddleware;
+use Platine\Framework\Http\Middleware\SecurityPolicyMiddleware;
 
     return [
         ErrorHandlerMiddleware::class,
         RouteMatchMiddleware::class,
         DemoMiddleware::class,
+        SecurityPolicyMiddleware::class,
         RouteDispatcherMiddleware::class,
     ];
