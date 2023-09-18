045321ae3927558337344165f8c817924b7f2c68
NGUEREZA Tony <nguerezatony@gmail.com>
2023-09-18 11:13:52 +0100

    Fix bug

--- config/middlewares.php
+++ config/middlewares.php
@@ -4,12 +4,11 @@ use Platine\App\Http\Middleware\DemoMiddleware;
 use Platine\Framework\Http\Middleware\ErrorHandlerMiddleware;
 use Platine\Framework\Http\Middleware\RouteDispatcherMiddleware;
 use Platine\Framework\Http\Middleware\RouteMatchMiddleware;
-use Platine\Framework\Http\Middleware\SecurityPolicyMiddleware;
 
     return [
         ErrorHandlerMiddleware::class,
         RouteMatchMiddleware::class,
         DemoMiddleware::class,
-        SecurityPolicyMiddleware::class,
+       // SecurityPolicyMiddleware::class,
         RouteDispatcherMiddleware::class,
     ];
