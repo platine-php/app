e63c1176e21dc9b0957c37585e1a136bb59d1c2a
NGUEREZA Tony <nguerezatony@gmail.com>
2023-11-20 04:26:02 +0100

    update maintenance configuration

--- config/maintenance.php
+++ config/maintenance.php
@@ -4,7 +4,10 @@
         'driver' => env('PL_APP_MAINTENANCE_DRIVER', 'file'),
         'bypass_route' => '',
         'url_whitelist' => [],
-        'cookie_lifetime' => env('PL_APP_MAINTENANCE_COOKIE_LIFETIME', 43200, 'int'),
+        'cookie' => [
+            'name' => env('PL_APP_MAINTENANCE_COOKIE_NAME', 'platine_maintenance'),
+            'lifetime' => env('PL_APP_MAINTENANCE_COOKIE_LIFETIME', 43200, 'int'),
+        ],
         'storages' => [
             'file' => [
                 'path'   => env('PL_MAINTENANCE_FILE_PATH', __DIR__ . '/../storage/maintenance'),
