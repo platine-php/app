8eee96af9aea2f25d96360b0dceaf2f2cc8856de
Tony NGUEREZA <nguerezatony@gmail.com>
2024-06-25 05:36:50 +0100

    Add webauthn package

---
+++ config/webauthn.php
@@ -0,0 +1,15 @@
+<?php
+
+    return [
+        'relying_party_id' => env('PL_WEBAUTHN_RELYING_PARTY_ID', 'localhost'),
+        'relying_party_name' => env('PL_WEBAUTHN_RELYING_PARTY_NAME', 'Platine App'),
+        'relying_party_logo' => '', // base64 image format
+        'timeout' => env('PL_WEBAUTHN_TIMEOUT', 60, 'int'), // in second
+        'challenge_length' => env('PL_WEBAUTHN_CHALLENGE_LENGTH', 32, 'int'), // in second
+        'transport_types' => [
+            'internal',
+        ],
+        'ignore_origins' => [
+            'localhost'
+        ],
+    ];
