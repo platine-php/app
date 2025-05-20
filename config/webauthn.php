ac023c0547af7d25b1c68ad191500728ea5b5438
Tony NGUEREZA <nguerezatony@gmail.com>
2025-05-20 21:05:09 +0100

    Minor change

--- config/webauthn.php
+++ config/webauthn.php
@@ -5,7 +5,7 @@
         'relying_party_name' => env('PL_WEBAUTHN_RELYING_PARTY_NAME', 'Platine App'),
         'relying_party_logo' => '', // base64 image format
         'timeout' => env('PL_WEBAUTHN_TIMEOUT', 60, 'int'), // in second
-        'challenge_length' => env('PL_WEBAUTHN_CHALLENGE_LENGTH', 32, 'int'), // in second
+        'challenge_length' => env('PL_WEBAUTHN_CHALLENGE_LENGTH', 32, 'int'),
         'transport_types' => [
             'internal',
         ],
